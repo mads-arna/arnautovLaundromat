f80a9de44a41e75bd955c1f5a487bcbcd4b4a6e1
mads <mads.arnautov@gmail.com>
2023-03-17 11:09:39 -0500

    home page is working mockup

---
+++ app.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+	const menu_btn = document.querySelector(".hamburger");
+	const mobile_menu = document.querySelector(".mobile-nav");
+	const fixed_nav = document.querySelector("body");
+
+	menu_btn.addEventListener("click", function () {
+		menu_btn.classList.toggle("is-active");
+		mobile_menu.classList.toggle("is-active");
+		fixed_nav.classList.toggle("is-fixed");
+	});
+};
