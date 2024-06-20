e9c7feffa73e4e08f19e383d5ad2d8ae43b46d29
Mads <mads.arnautov@gmail.com>
2024-06-20 17:12:08 -0400

    added pixel snippet for ad

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-window.onload = function () {
-	const menu_btn = document.querySelector(".hamburger");
-	const mobile_menu = document.querySelector(".mobile-nav");
-	const fixed_nav = document.querySelector("body");
+<script
+  async
+  src="https://tag.simpli.fi/sifitag/bc87abb9-b015-44df-93b8-78e2f7d32006"></script>;
 
-	menu_btn.addEventListener("click", function () {
-		menu_btn.classList.toggle("is-active");
-		mobile_menu.classList.toggle("is-active");
-		fixed_nav.classList.toggle("is-fixed");
-	});
+window.onload = function () {
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+  const fixed_nav = document.querySelector("body");
+
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+    fixed_nav.classList.toggle("is-fixed");
+  });
 };
